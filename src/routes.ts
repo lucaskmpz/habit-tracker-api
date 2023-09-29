b826a40a0a3250a6564a9e9a3758db727ab3dee7
Lucas <lucasclmaia0@gmail.com>
2023-09-29 10:19:53 -0300

    bug fixing seed and schema

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,19 @@
 import { FastifyInstance } from "fastify"
 import { z } from "zod"
 import { prisma } from "./lib/prisma"
+import dayjs from "dayjs"
 
 export async function appRoutes(app: FastifyInstance) {
-    app.post("/habits", async () => {
+    app.post("/habits", async (request) => {
         const createHabitBody = z.object({
             title: z.string(),
             weekDays: z.array(
                 z.number().min(0).max(6)
-            )
+            ),
         })
 
         const { title, weekDays } = createHabitBody.parse(request.body)
+        console.log(request.body)
 
         const today = dayjs().startOf("day").toDate()
 
